587381ee8c0c6b9116d4f40a7d98e99eb41e63d0
YounQsheyex <younqsheyex@gmail.com>
2025-06-28 19:32:37 -0700

    just saving

--- src/components/UpdateStoryForm.jsx
+++ src/components/UpdateStoryForm.jsx
@@ -8,10 +8,7 @@ const UpdateStoryForm = () => {
     setOptions(e.target.value);
   };
   return (
-    <div className="w-sm lg:w-[1240px] mx-auto p-5 lg:p-0">
-      <h1 className="w-[292px] font-[harmattan] font-[700] text-[40px] lg:text-[60px] text-[#292929]">
-        Create Story
-      </h1>
+    <div className="w-sm lg:w-[1240px] mx-auto p-5 lg:p-0 mt-7">
       <form>
         <div className="relative">
           <input
@@ -57,8 +54,7 @@ const UpdateStoryForm = () => {
           ></label>
           <textarea
             id="story"
-            placeholder="Write your story......."
-            className="w-full lg:w-[1239px] h-[568px] rounded-[5px] border-[1px] border-[#dddddd] placeholder:text-[24px] placeholder:font-[700] placeholder:font-[harmattan] placeholder:text-[#757575] px-4 py-2 focus:outline-none"
+            className="w-full lg:w-[1239px] h-[568px] rounded-[5px] border-[1px] border-[#dddddd] px-4 py-2 focus:outline-none"
           ></textarea>
         </div>
         <div className="mt-5 flex items-center justify-center">
